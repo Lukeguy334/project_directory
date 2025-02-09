1e7a46ce76fb8aa316d563897a9a769c044e2634
Lukeguy334 <lhs57@usf.edu>
2025-02-08 23:22:34 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,5 @@
+// Task 1 - Customer Profile
+let customer = { name: "John Doe", age: 35, email: "john.doe@email.com" }
+console.log(customer.name); // John Doe
+console.log(customer.age); // 35
+console.log(customer.email); //
